a2995e3e7ee07ced8c99ae35d39285b5ba228e10
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-07-27 20:56:42 +0500

    error of undefined value corrected

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ button.addEventListener("click", getFIO);
 function getFIO() {
     let input = document.querySelector(".fio").value;
     
-    // преобразует к нижнему регитспу, в случае если пользователь ввел большими буквами
+    // преобразует к нижнему регистру, в случае если пользователь ввел большими буквами
     let lowerCaseInput = input.toLowerCase();
 
     // удаляет пробелы в начале и конце строки
@@ -12,21 +12,24 @@ function getFIO() {
     
     // делает заглавной каждую букву слова в строке 
     let newInput = deleteSpaces.replace(/[a-zа-я]+/gi, (match) => match[0].toUpperCase() + match.substr(1));
-    
+
     // разбивает строку на массив 
     let array = newInput.split(" ");
-    
+
+    if (array.length > 2) {
     let i = 0;
     getData(".surname");
     i++;
     getData(".name");
     i++
     getData(".middle-name");
+    function getData(n) {
+        document.querySelector(n).value = array[i];   
+    }
+    }
+    else {
+        return 0;
+    }
 
     input = document.querySelector(".fio").value = " ";
-
-    function getData(n) {
-        document.querySelector(n).value = array[i];
-    }
 }
-
