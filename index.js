809230d07f038ba2f32a1f0c891e686360209c4e
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-07-27 22:08:56 +0500

    html tags moved to js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let fio = document.createElement("div");
+fio.classList.add("container");
+fio.innerHTML = `<label for="fio">Пожалуйста, введите ФИО:
+<input class="fio" id="fio" type="text" />
+<button type="button">Готово</button></label>`;
+document.body.append(fio);
+
 const button = document.querySelector("button");
 button.addEventListener("click", getFIO);
 
@@ -15,21 +22,31 @@ function getFIO() {
 
     // разбивает строку на массив 
     let array = newInput.split(" ");
-
+    
     if (array.length > 2) {
-    let i = 0;
-    getData(".surname");
-    i++;
-    getData(".name");
-    i++
-    getData(".middle-name");
-    function getData(n) {
+        let result = document.createElement("div");
+        result.classList.add("result");
+        result.innerHTML = `<div>
+        <label for="surname">Фамилия:</label>
+        <input class="surname" type="text" id="surname" /></div>
+        <div><label for="name">Имя:</label>
+        <input class="name" type="text" id="name" /></div>
+        <div><label for="middle-name">Отчество:</label>
+        <input class="middle-name" type="text" id="middle-name" /></div>`;
+        document.body.append(result);
+        let i = 0;
+        getData(".surname");
+        i++;
+        getData(".name");
+        i++
+        getData(".middle-name");
+        function getData(n) {
         document.querySelector(n).value = array[i];   
-    }
+        }
     }
     else {
         return 0;
     }
 
-    input = document.querySelector(".fio").value = " ";
+    input = document.querySelector(".fio").value = "";   
 }
